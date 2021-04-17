3fecef49d11c061c9f61d530f05a9102a37d6316
cauã <48422051+cauaajk@users.noreply.github.com>
2021-04-17 03:46:54 -0300

    Update client.lua

--- gdl_marker-master/client.lua
+++ gdl_marker-master/client.lua
@@ -1,3 +1,6 @@
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- FUNÇÕES
+-----------------------------------------------------------------------------------------------------------------------------------------
 local Blips = {
     -- Exemplo:
 	{name="EXEMPLO", x = -617.99, y = -1589.58, z = 26.75, id= 1, color= 39, scale = 0.5},
@@ -19,11 +22,13 @@ end)
 
 -- Blips Visuais
 Citizen.CreateThread(function()
-		while true do
-	local timeDistance = 500
-	timeDistance = 4
+	while true do
+        local timeDistance = 500
+        timeDistance = 4
 		-- Exemplo:
-		DrawMarker(36, -775.25,5583.54,33.48 -0.20, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 1.00, 2.00, 1.00, 255, 178, 102, 200, false, true, 2, true, false, false, false)
+		DrawMarker(36, --[[Coordenada]] -775.25,5583.54,33.48 -0.20, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 1.00, 2.00, 1.00, 255, 178, 102, 200, false, true, 2, true, false, false, false)
+
+		Citizen.Wait(timeDistance)
 	end
 end)
 -- número dos blips: https://forum.fivem.net/t/resource-available-markers/99384
